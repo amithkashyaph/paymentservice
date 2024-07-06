c00ae46cf630068395a7d859d11e8be7b9a31cec
amithkashyap <amithkashyaprv@gmail.com>
2024-07-06 18:12:28 +0530

    added initial setup for getPaymentLink API

--- src/main/java/com/ecommerce/paymentservice/controllers/PaymentController.java
+++ src/main/java/com/ecommerce/paymentservice/controllers/PaymentController.java
@@ -1,9 +1,24 @@
 package com.ecommerce.paymentservice.controllers;
 
+import com.ecommerce.paymentservice.dtos.InitiatePaymentRequestDto;
+import com.ecommerce.paymentservice.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
+
+    private PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public String getPaymentLink(@RequestBody InitiatePaymentRequestDto initiatePaymentRequestDto) {
+        return null;
+    }
 }
