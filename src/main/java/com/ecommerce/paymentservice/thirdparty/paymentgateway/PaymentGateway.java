5e545059136ceed607a5d51536ad859b1356ace5
amithkashyap <amithkashyaprv@gmail.com>
2024-07-06 18:17:57 +0530

    updated getPaymentLink function signature in Payment gateway interface

--- src/main/java/com/ecommerce/paymentservice/thirdparty/paymentgateway/PaymentGateway.java
+++ src/main/java/com/ecommerce/paymentservice/thirdparty/paymentgateway/PaymentGateway.java
@@ -3,5 +3,5 @@ package com.ecommerce.paymentservice.thirdparty.paymentgateway;
 import com.razorpay.RazorpayException;
 
 public interface PaymentGateway {
-    String getPaymentLink(String orderId, Long amount) throws RazorpayException;
+    String getPaymentLink(String orderId, Long amount, String phoneNumber, String email) throws RazorpayException;
 }
