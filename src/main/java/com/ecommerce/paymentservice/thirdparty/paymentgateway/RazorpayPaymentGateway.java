e11ec23e6534f33db2c3b2140c84a154388041c1
amithkashyap <amithkashyaprv@gmail.com>
2024-07-06 16:18:49 +0530

    added Razorpay client logic

--- src/main/java/com/ecommerce/paymentservice/thirdparty/paymentgateway/RazorpayPaymentGateway.java
+++ src/main/java/com/ecommerce/paymentservice/thirdparty/paymentgateway/RazorpayPaymentGateway.java
@@ -1,10 +1,41 @@
 package com.ecommerce.paymentservice.thirdparty.paymentgateway;
 
 import com.ecommerce.paymentservice.thirdparty.paymentgateway.PaymentGateway;
+import com.razorpay.PaymentLink;
+import org.json.JSONObject;
+import com.razorpay.Payment;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
 
 public class RazorpayPaymentGateway implements PaymentGateway {
     @Override
-    public String getPaymentLink(String orderId, Long amount) {
+    public String getPaymentLink(String orderId, Long amount) throws RazorpayException {
+        RazorpayClient razorpay = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
+        JSONObject paymentLinkRequest = new JSONObject();
+        paymentLinkRequest.put("amount",1000);
+        paymentLinkRequest.put("currency","INR");
+        paymentLinkRequest.put("accept_partial",true);
+        paymentLinkRequest.put("first_min_partial_amount",100);
+        paymentLinkRequest.put("expire_by",1691097057);
+        paymentLinkRequest.put("reference_id","TS1989");
+        paymentLinkRequest.put("description","Payment for policy no #23456");
+        JSONObject customer = new JSONObject();
+        customer.put("name","+919000090000");
+        customer.put("contact","Gaurav Kumar");
+        customer.put("email","gaurav.kumar@example.com");
+        paymentLinkRequest.put("customer",customer);
+        JSONObject notify = new JSONObject();
+        notify.put("sms",true);
+        notify.put("email",true);
+        paymentLinkRequest.put("notify",notify);
+        paymentLinkRequest.put("reminder_enable",true);
+        JSONObject notes = new JSONObject();
+        notes.put("policy_name","Jeevan Bima");
+        paymentLinkRequest.put("notes",notes);
+        paymentLinkRequest.put("callback_url","https://example-callback-url.com/");
+        paymentLinkRequest.put("callback_method","get");
+
+        PaymentLink payment = razorpay.paymentLink.create(paymentLinkRequest);
         return null;
     }
 }
