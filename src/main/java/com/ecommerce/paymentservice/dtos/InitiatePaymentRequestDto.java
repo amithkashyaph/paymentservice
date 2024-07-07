f7057298c7eccf21366ec0304a33e3b00ed10b77
amithkashyap <amithkashyaprv@gmail.com>
2024-07-08 00:28:58 +0530

    updated initiate oayment request dto

--- src/main/java/com/ecommerce/paymentservice/dtos/InitiatePaymentRequestDto.java
+++ src/main/java/com/ecommerce/paymentservice/dtos/InitiatePaymentRequestDto.java
@@ -6,4 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class InitiatePaymentRequestDto {
+     private String orderId;
+     private Long amount;
+     private String phoneNumber;
+     private String email;
 }
