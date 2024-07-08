5f2d0f533cc14d9f91b4db0bd5af04c0edf0102c
amithkashyap <amithkashyaprv@gmail.com>
2024-07-08 23:35:53 +0530

    added payment config class

--- src/main/java/com/ecommerce/paymentservice/config/PaymentGatewayConfig.java
+++ src/main/java/com/ecommerce/paymentservice/config/PaymentGatewayConfig.java
@@ -1,7 +1,18 @@
 package com.ecommerce.paymentservice.config;
 
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PaymentGatewayConfig {
+
+    private String clientId;
+    private String clientSecret;
+
+    @Bean
+    public RazorpayClient createRazorpayClient() throws RazorpayException {
+        return new RazorpayClient(clientId, clientSecret);
+    }
 }
